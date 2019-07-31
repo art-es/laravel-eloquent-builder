ebd3dbe40b07f429e44ac07b8927c0fbe947e821
art-es <sh.artur.ilf@gmail.com>
2019-07-31 15:47:55 +0300

    remove deprecated files clear LaravelBuilderTest

--- src/Base/LaravelBuilderServiceProvider.php
+++ src/Base/LaravelBuilderServiceProvider.php
@@ -7,6 +7,12 @@ use Artes\LaravelBuilder\Base\Commands\ModelMakeCommand;
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class LaravelBuilderServiceProvider
+ * Add make:builder command to artisan console.
+ *
+ * @package Artes\LaravelBuilder\Base
+ */
 class LaravelBuilderServiceProvider extends ServiceProvider
 {
     public function boot(Router $router): void
--- tests/LaravelBuilderTest.php
+++ tests/LaravelBuilderTest.php
@@ -5,34 +5,15 @@ use Illuminate\Support\Facades\Artisan;
 
 class LaravelBuilderTest extends TestCase
 {
-//    /**
-//     * Just check if the EloquentModel class has no syntax error
-//     *
-//     * @return void
-//     */
-//    public function testUpdatingSqlQuery(): void
-//    {
-//    }
-//
-//    /**
-//     * Check for making model where model extends of EloquentModel class
-//     *
-//     * @return void
-//     */
-//    public function testsMakeModelCommand(): void
-//    {
-//        $this->artisan('make:model Post');
-//    }
-//
-//    /**
-//     * Creates the application.
-//     *
-//     * Needs to be implemented by subclasses.
-//     *
-//     * @return \Symfony\Component\HttpKernel\HttpKernelInterface
-//     */
-//    public function createApplication()
-//    {
-//        // TODO: Implement createApplication() method.
-//    }
+    /**
+     * Creates the application.
+     *
+     * Needs to be implemented by subclasses.
+     *
+     * @return \Symfony\Component\HttpKernel\HttpKernelInterface
+     */
+    public function createApplication()
+    {
+        // TODO: Implement createApplication() method.
+    }
 }
